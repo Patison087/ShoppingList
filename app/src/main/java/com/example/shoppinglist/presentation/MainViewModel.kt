e56419af51af3f3f3401730e85207dad944b30c9
patison087 <patison087@gmail.com>
2022-01-26 18:10:14 +0300

    lesson 4 LiveData ViewModel

--- app/src/main/java/com/example/shoppinglist/presentation/MainViewModel.kt
+++ app/src/main/java/com/example/shoppinglist/presentation/MainViewModel.kt
@@ -13,19 +13,13 @@ class MainViewModel : ViewModel () {
     private val deleteShopItemUseCase = DeleteShopItemUseCase (repository)
     private val editShopListUseCase = EditShopItemUseCase (repository)
 
-    val shopList = MutableLiveData<List<ShopItem>> ()
+    val shopList = getShopListUseCase.getShopList()
 
-    fun getShopList(){
-        val list = getShopListUseCase.getShopList()
-        shopList.value = list
-    }
     fun deleteShopItem (shopItem: ShopItem) {
         deleteShopItemUseCase.deleteShopItem(shopItem)
-        getShopList()
     }
     fun changeEnabledState(shopItem: ShopItem){
         val newItem = shopItem.copy(enabled = !shopItem.enabled)
         editShopListUseCase.editShopItem(newItem)
-        getShopList()
     }
 }
